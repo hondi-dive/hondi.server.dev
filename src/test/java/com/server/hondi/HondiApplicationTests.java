1efb3809747e674f82c369ac978881683c63ba8a
kimjaehyuk <jhkim0097@gmail.com>
2023-08-07 18:24:57 +0900

    init boot

---
+++ src/test/java/com/server/hondi/HondiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.hondi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HondiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
